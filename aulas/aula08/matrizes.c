79412eb48cedd8433653b0190073a18c5cbdd2c3
Vinícius <viniciusoliveira120508@gmail.com>
2026-05-20 21:45:25 -0300

    codigos da aula08

---
+++ aulas/aula08/matrizes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    int tabela[3][3];
+    //  0 1 2
+    //0| | | |
+    //1| | | |
+    //2| | | |
+
+    tabela[1][2] = 0;
+    //  0 1 2
+    //0| | | |
+    //1| | |0|
+    //2| | | |
+
+    printf("Preencha a tabela 3x3: \n");
+    // percorre as linhas
+    for(int i = 0; i < 3; i++){
+        //percorre as colunas
+        for(int j = 0; j < 3; j++){
+            printf("Entre com um valor para a posicao [%i, %i]: ", i, j);
+            scanf("%i", &tabela[i][j]);
+        }
+    }
+
+    printf("Sua tabela ficou assim: \n");
+    // percorre as linhas
+    for(int i = 0; i < 3; i++){
+        //percorre as colunas
+        for(int j = 0; j < 3; j++){
+            printf("%i ", tabela[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+    return 0;
+}
